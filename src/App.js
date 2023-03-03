7a6eb09a03bd9b0ae1f594058bcdb4a8de59a825
Chandan-repository <67368366+Chandan-repository@users.noreply.github.com>
2023-03-03 12:31:13 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
+import React ,{ useState } from 'react';
 
-import './style.css';
+import './styles.css';
 
 // don't change the Component name "App"
 export default function App() {
-  const [messageValidity, setMessageValidity] = useState('Invalid');
-  const clickHandler = (event) => {
-    const value = event.target.value;
-    if (value.length < 3) {
-      setMessageValidity('Invalid');
-    } else {
-      setMessageValidity('Valid');
-    }
-  };
-  return (
-    <form>
-      <label>Your message</label>
-      <input type="text" onChange={clickHandler} />
-      <p>{messageValidity} message</p>
-    </form>
-  );
+    const [counter,setCounter]=useState(0);
+    const incrementCounterValue=() =>{
+        setCounter(prevState => prevState + 1);
+    };
+    return (
+      <div>
+        <p id="counter">{counter}</p>
+        <button onClick={incrementCounterValue}>Increment</button>
+      </div>
+    );
 }
