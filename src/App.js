9ae93304165ef1e4d1b2f6a0a43a6c26e6b8b4c6
Chandan-repository <67368366+Chandan-repository@users.noreply.github.com>
2023-03-03 12:35:19 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React ,{ useState } from 'react';
+import React, { useState } from 'react';
 
-import './styles.css';
+import './style.css';
 
 // don't change the Component name "App"
 export default function App() {
-    const [counter,setCounter]=useState(0);
-    const incrementCounterValue=() =>{
-        setCounter(prevState => prevState + 1);
-    };
-    return (
-      <div>
-        <p id="counter">{counter}</p>
-        <button onClick={incrementCounterValue}>Increment</button>
-      </div>
-    );
+  const [counter, setCounter] = useState(0);
+  const incrementCounterValue = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+  return (
+    <div>
+      <p id="counter">{counter}</p>
+      <button onClick={incrementCounterValue}>Increment</button>
+    </div>
+  );
 }
